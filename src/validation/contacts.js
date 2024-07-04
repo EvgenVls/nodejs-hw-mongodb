954b52ae19de610d68b61e66804e4e7ad11a6a3a
EvgenVls <145988014+EvgenVls@users.noreply.github.com>
2024-07-04 14:15:05 +0300

    update validation/contacts

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -6,7 +6,7 @@ export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().email(),
-  isFavourite: Joi.boolean().required(),
+  isFavourite: Joi.boolean(),
   contactType: Joi.string()
     .valid(...contactTypeList)
     .required(),
