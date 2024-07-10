aa64cb0035dd7e232b0021eb901f7a4d9fdc5928
Yevgen Vlasenko <evgenvl@ukr.net>
2024-07-10 07:20:31 +0300

    step 2: create models user and session

---
+++ src/db/models/users.js
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose';
+
+import { emailRegexp } from '../../constants/users.js';
+
+import { mongooseSaveError, setUpdateSettings } from './hooks.js';
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      match: emailRegexp,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+userSchema.post('save', mongooseSaveError);
+
+userSchema.pre('findOneAndUpdate', setUpdateSettings);
+
+userSchema.post('findOneAndUpdate', mongooseSaveError);
+
+export const User = model('user', userSchema);
